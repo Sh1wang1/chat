02b1df43537e6cf370f42d2fd2b8e29ffb12a8a1
Shiwangi Verma <shiwangiverma2003@gmail.com>
2025-07-21 17:51:17 +0530

    all thing worked

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,11 +20,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
